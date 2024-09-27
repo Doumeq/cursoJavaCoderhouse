ed0cd9b441f10fc508ee29c85ef76a54fdaccda1
Doumeq <f.nicolasdomeq@gmail.com>
2024-09-27 00:28:41 -0300

    Subida de las clases 00, 01 y un ejemplo de creacion de un project

--- clase-00/src/main/java/com/coderhouse/Clase00.java
+++ clase-00/src/main/java/com/coderhouse/Clase00.java
@@ -3,7 +3,7 @@ package com.coderhouse;
 public class Clase00 {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo"); //Para imprimir por consola se usa "syso" + ctrl y espacio o escribir System.ou.println("")
+		System.out.println("Hola mundo"); //Para imprimir en la consola se usa "syso" + ctrl y espacio o escribir System.ou.println("")
 	}
 
 }
--- clase-01/src/main/java/com/coderhouse/Clase01.java
+++ clase-01/src/main/java/com/coderhouse/Clase01.java
@@ -2,6 +2,6 @@ package com.coderhouse;
 
 public class Clase01 {
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
+		System.out.println("Hola mundo");//
 	}
 }
