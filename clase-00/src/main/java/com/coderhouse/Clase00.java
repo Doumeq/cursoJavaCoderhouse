93c8c65e09d2e6975a72cfa965d53becddcc5a6c
Doumeq <f.nicolasdomeq@gmail.com>
2024-09-27 00:32:22 -0300

    primera subida de las clases 0 y 1

--- clase-00/src/main/java/com/coderhouse/Clase00.java
+++
@@ -1,9 +0,0 @@
-package com.coderhouse;
-
-public class Clase00 {
-
-	public static void main(String[] args) {
-		System.out.println("Hola mundo"); //Para imprimir en la consola se usa "syso" + ctrl y espacio o escribir System.ou.println("")
-	}
-
-}
