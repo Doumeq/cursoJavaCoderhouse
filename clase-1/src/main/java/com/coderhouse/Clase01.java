a29445aab779e2134a2dde201010ef23be6585f8
Doumeq <f.nicolasdomeq@gmail.com>
2024-09-30 21:07:52 -0300

    Actividad clase 01

--- clase-1/src/main/java/com/coderhouse/Clase01.java
+++ clase-1/src/main/java/com/coderhouse/Clase01.java
@@ -2,6 +2,17 @@ package com.coderhouse;
 
 public class Clase01 {
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");//
+		int capitalSolicitado = 12000;
+		int interes = 5;
+		int periodoEnMeses = 3;
+		
+		
+		int interesAPagar = capitalSolicitado * interes * periodoEnMeses;
+		
+		
+		
+		int montoTotal = capitalSolicitado + interesAPagar;
+		
+		System.out.println("El monto total a pagar es: " + montoTotal);
 	}
 }
