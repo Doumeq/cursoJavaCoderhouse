684a1690e7be6c61dc37d5197513dc4f4a1c3d5d
Doumeq <f.nicolasdomeq@gmail.com>
2024-10-03 00:05:44 -0300

    primera parte de ejercicios de la clase 2

---
+++ clase-2/src/main/java/com/coderhouse/Ejercicios.java
@@ -0,0 +1,85 @@
+package com.coderhouse;
+
+public class Ejercicios {
+
+	public static void main(String[] args) {
+		//Determina si un número es positivo. Declara un número y usa if para verificar si es mayor que 0.
+		/*
+		int numero = -3;
+		
+		if(numero>=0){
+			System.out.println("Este numero es positivo");
+		} else {
+			System.out.println("Este numero es negativo");
+		}
+		*/
+		
+		//Verifica si una persona es mayor de edad (>= 18 años).
+		/*
+		int edadUsuario = 17;
+		if(edadUsuario >= 18){
+			System.out.println("El usuario es mayor de edad");
+		} else {
+			System.out.println("El usuario es menor de edad");
+		}
+		*/
+		
+		//Comprueba si un número es impar usando el operador %.
+		/*
+		int numero = 9;
+		if(numero % 2 == 0){
+			System.out.println("Es par");
+		} else {
+			System.out.println("Es impar");
+		}
+		*/
+		
+		//Revisa si una cadena está vacía usando if con string.isEmpty().
+		/*
+		String texto = "";
+		
+		if(texto.isEmpty()) {
+			System.out.println("Este texto esta vacio");
+		} else {
+			System.out.println("Este texto contiene datos");
+		}
+		*/
+		
+		//Determina si un número es divisible por 5.
+		/*
+		int numero = 250;
+		
+		if(numero % 5 == 0){
+			System.out.println("Este numero es divisible por 5");
+		} else {
+			System.out.println("Este numero no es divisible por 5");
+		}
+		*/
+		
+		//Verifica si un número es par o impar y muestra un mensaje correspondiente.
+		/*
+		int numero = 8;
+		if(numero % 2 == 0){
+			System.out.println("Es par");
+		} else {
+			System.out.println("Es impar");
+		}
+		*/
+		
+		//Determina si una persona puede votar (mayor de 18 años) o no.
+		/*
+		int persona = 19;
+		
+		if(persona>=18){
+			System.out.println("Puede votar");
+		} else {
+			System.out.println("No puede votar");
+		}
+		*/
+		
+		//Verifica si una letra es vocal o consonante (para simplificar, considera solo las letras en minúsculas).
+		
+	}
+	
+
+}
